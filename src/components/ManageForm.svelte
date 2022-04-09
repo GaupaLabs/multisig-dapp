59b3dada499e8885248e4d3027fa28e5be127aac
Lukas Schild <47003752+luuuuk@users.noreply.github.com>
2022-04-09 20:56:43 +0200

    feat: tx manage form

--- src/components/ManageForm.svelte
+++ src/components/ManageForm.svelte
@@ -1,40 +1,106 @@
 <script>
-	let proposedAddress = 'erd1';
+	import { contractAddress, baseHookURL } from '../stores.js';
+	import { bech32 } from 'bech32';
 
-	function proposeManage() {}
+	let proposedAddress = 'erd1';
+	let selectedOption;
+	let newQuorum;
+
+	let proposalCommands = [
+		'proposeAddBoardMember',
+		'proposeAddProposer',
+		'proposeRemoveUser',
+		'proposeChangeQuorum'
+	];
+
+	function proposeManage() {
+		console.log(selectedOption.toString());
+	}
+
+	function proposeSending() {
+		let requestData;
+
+		if (selectedOption == 3) {
+			requestData = proposalCommands[selectedOption] + '@' + newQuorum.toString(16);
+		} else {
+			let parsedAddress = proposedAddress == 'erd1' ? 0x00 : convertAddressToHex(proposedAddress);
+			requestData = proposalCommands[selectedOption] + '@' + parsedAddress;
+		}
+
+		let request =
+			$baseHookURL +
+			'transaction?receiver=' +
+			$contractAddress +
+			'&value=0&data=' +
+			requestData +
+			'&callbackUrl=http://localhost:3000/dashboard';
+
+		console.log(request);
+
+		window.open(request);
+	}
+
+	function convertAddressToHex(address) {
+		let parsedWords = bech32.fromWords(bech32.decode(address).words);
+		let parsedAddress = '';
+
+		parsedWords.forEach((item, _index) => {
+			let converted = String(item.toString(16)).padStart(2, '0');
+			parsedAddress += converted;
+		});
+
+		return parsedAddress;
+	}
 </script>
 
 <div class="row align-items-end" style="padding: 2em;">
 	<div class="col">
 		<h4>Manage board members, quorum and action proposer.</h4>
-		<p style="margin-top: 2em;">Choose the action and address the proposal concerns.</p>
+		<p style="margin-top: 2em;">Choose the action and address or quorum the proposal concerns.</p>
 	</div>
 </div>
 <div class="row align-items-end" style="margin-top: -4em; padding: 2em;">
 	<div class="col-lg-3">
 		<label for="Action option">Action</label>
-		<select class="form-select" aria-label="Action option">
+		<select class="form-select" aria-label="Action option" bind:value={selectedOption}>
 			<option selected>Select an option</option>
-			<option value="1">Add Board Member</option>
-			<option value="2">Add Proposer</option>
-			<option value="3">Remove User</option>
-			<option value="4">Change Quorum</option>
+			<option value="0">Add Board Member</option>
+			<option value="1">Add Proposer</option>
+			<option value="2">Remove User</option>
+			<option value="3">Change Quorum</option>
 		</select>
 	</div>
-	<div class="col-lg-9">
-		<label for="amount">Address</label>
-		<div class="input-group">
-			<input
-				type="text"
-				class="form-control"
-				id="address"
-				placeholder="Address"
-				aria-label="Address"
-				bind:value={proposedAddress}
-			/>
-			<span class="input-group-text">ERD Adress</span>
+	{#if selectedOption == 3}
+		<div class="col-lg-6">
+			<label for="quorum">Quorum</label>
+			<div class="input-group">
+				<input
+					type="number"
+					class="form-control"
+					id="quorum"
+					placeholder="Quorum"
+					aria-label="Quorum"
+					bind:value={newQuorum}
+				/>
+				<span class="input-group-text">Number of Signatures Needed</span>
+			</div>
 		</div>
-	</div>
+	{:else}
+		<div class="col-lg-9">
+			<label for="amount">Address</label>
+			<div class="input-group">
+				<input
+					type="text"
+					class="form-control"
+					id="address"
+					placeholder="Address"
+					aria-label="Address"
+					bind:value={proposedAddress}
+				/>
+				<span class="input-group-text">ERD Adress</span>
+			</div>
+		</div>
+	{/if}
 </div>
 <div class="row" style="padding: 0 2em 2em 2em;">
 	<div class="col">
