7ca5c1cd1d711ed2b873ad87b8ff6459cb70e5d8
Lukas Schild <47003752+luuuuk@users.noreply.github.com>
2022-04-10 15:42:46 +0200

    feat: account and contract info

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,17 +1,71 @@
 <script>
 	import { page } from '$app/stores';
 	import { contractAddress } from '../stores.js';
+	import { onMount } from 'svelte';
 	import SendForm from '../components/SendForm.svelte';
 	import ActionsForm from '../components/ActionsForm.svelte';
 	import ManageForm from '../components/ManageForm.svelte';
 	import DepositForm from '../components/DepositForm.svelte';
+	import { each } from 'svelte/internal';
 
 	const userAddress = $page.url.searchParams.get('address');
 	const lastTx = $page.url.searchParams.get('txHash');
 
+	let walletAddress = '';
+	let walletBalance = 0;
+	let walletNonce = 0;
+	let multisigAddress = '';
+	let multisigBalance = 0;
+	let multisigRewards = 0;
+	let lastTransactions = [];
+
 	let selectedTab = 'send';
-	let amountEGLD = 0;
-	function addFunds() {}
+
+	onMount(async function () {
+		if (userAddress) {
+			const response = await fetch('https://devnet-api.elrond.com/accounts/' + userAddress);
+			const userData = await response.json();
+
+			walletAddress = userData['address'];
+			walletBalance = userData['balance'] / 10 ** 18;
+			walletNonce = userData['nonce'];
+		}
+
+		if ($contractAddress) {
+			const response = await fetch('https://devnet-api.elrond.com/accounts/' + $contractAddress);
+			const contractData = await response.json();
+
+			multisigAddress = contractData['address'];
+			multisigBalance = contractData['balance'] / 10 ** 18;
+			multisigRewards = contractData['developerReward'] / 10 ** 18;
+
+			const txResponse = await fetch(
+				'https://devnet-api.elrond.com/accounts/' +
+					$contractAddress +
+					'/transactions?size=5&withLogs=false'
+			);
+			const txData = await txResponse.json();
+
+			txData.forEach((element) => {
+				let tx = {
+					txHash: element['txHash'],
+					value: element['value'] / 10 ** 18,
+					status: element['status'],
+					timestamp: element['timestamp'],
+					sender: element['sender']
+				};
+
+				if (element['action']) {
+					tx['action'] = element['action']['name'];
+				}
+
+				lastTransactions = [...lastTransactions, tx];
+			});
+		}
+
+		if (lastTx) {
+		}
+	});
 </script>
 
 <div class="container hero-dark text-light">
@@ -21,16 +75,31 @@
 			<p>Find all the contract interaction shortcuts on this page.</p>
 		</div>
 	</div>
-	<div class="row justify-content-center align-items-center" style="margin-top: 2em;">
-		<div class="col">
-			<h4>Your Address</h4>
-			<p class="address-card text-dark">{userAddress}</p>
+</div>
+
+<div class="container hero-dark text-light">
+	<div class="row justify-content-center align-items-stretch">
+		<div class="col-lg-6">
+			<h4>Your Account</h4>
+			<div class="account-card text-dark">
+				<h5>Address</h5>
+				<p class="long-field">{walletAddress}</p>
+				<h5>Balance</h5>
+				<p>{walletBalance} EGLD</p>
+				<h5>Current Nonce</h5>
+				<p>{walletNonce}</p>
+			</div>
 		</div>
-	</div>
-	<div class="row justify-content-center align-items-center" style="margin-top: 2em;">
-		<div class="col">
-			<h4>Contract Address</h4>
-			<p class="address-card text-dark">{$contractAddress}</p>
+		<div class="col-lg-6">
+			<h4>Contract</h4>
+			<div class="account-card text-dark">
+				<h5>Address</h5>
+				<p class="long-field">{multisigAddress}</p>
+				<h5>Balance</h5>
+				<p>{multisigBalance} EGLD</p>
+				<h5>Developer Rewards</h5>
+				<p>{multisigRewards} EGLD</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -72,27 +141,41 @@
 	</div>
 </div>
 
-{#if lastTx}
-	<div class="container hero text-dark" style="margin-top: -4em;">
+{#if lastTransactions.length > 0}
+	<div class="container hero-dark text-light" style="margin-top: 2em;">
 		<div class="row">
 			<div class="col">
-				<h3>Last Transaction</h3>
-				<p style="margin-top: 1em;">Transaction hash: {lastTx}</p>
-				<p>
-					<a href="https://devnet-explorer.elrond.com/transactions/{lastTx}" target="_blank"
-						>View in explorer</a
-					>
-				</p>
+				<h3>Last Transactions with Smart Contract</h3>
+
+				{#each lastTransactions as tx}
+					<div class="account-card text-dark" style="margin-top: 2em;">
+						<h5 class="long-field">Hash: {tx['txHash']}</h5>
+						<p class="long-field" style="margin-top: 1em;">Sender: {tx['sender']}</p>
+						<p>Status: {tx['status']}</p>
+						<p>Value: {tx['value']}</p>
+
+						{#if tx['action']}
+							<p>Action: {tx['action']}</p>
+						{/if}
+
+						<p>
+							<a
+								href="https://devnet-explorer.elrond.com/transactions/{tx['txHash']}"
+								target="_blank">View in explorer</a
+							>
+						</p>
+					</div>
+				{/each}
 			</div>
 		</div>
 	</div>
 {/if}
 
 <style>
-	.address-card {
+	.account-card {
 		border-radius: 1em;
 		background-color: var(--background-white);
-		padding: 1em;
+		padding: 1.5em 1.5em 0.5em 1.5em;
 	}
 
 	.nav-item:hover {
@@ -101,5 +184,9 @@
 
 	.form-wrapper {
 		padding: 2em;
+	}
+
+	.long-field {
+		overflow: auto;
 	}
 </style>
