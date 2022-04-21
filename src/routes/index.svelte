0fb7b3fa98fbdffe3b27bbeaf9c43ab271636b42
Lukas Schild <47003752+luuuuk@users.noreply.github.com>
2022-04-21 12:50:30 +0200

    feat: resource cards

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,7 +13,7 @@
 			>
 		</div>
 		<div class="col-lg-4">
-			<img src="/static/lock.png" alt="dashboard icon" style="height:20em;" />
+			<img src="/static/lock.png" alt="dashboard icon" style="height: 20em;" />
 		</div>
 	</div>
 </div>
@@ -25,7 +25,7 @@
 		</div>
 	</div>
 	<div class="row justify-content-evenly align-items-stretch" style="margin-top: 4em;">
-		<div class="col-lg-5 dark-card text-light centered">
+		<div class="col-lg-5 dark-card text-light centered info-card">
 			<center style="margin-top: 2em;">
 				<img src="/static/chain-interact.png" alt="dashboard icon" style="height:8em;" />
 				<p class="large-text" style="margin-top: 2em;">Interact with existing contract</p>
@@ -34,7 +34,7 @@
 		<div class="col-lg-2 centered">
 			<h2 style="margin-top: 2em;">or</h2>
 		</div>
-		<div class="col-lg-5 dark-card text-light centered">
+		<div class="col-lg-5 dark-card text-light centered info-card">
 			<center style="margin-top: 2em;">
 				<img
 					src="/static/smart-contracts.png"
@@ -64,33 +64,42 @@
 </div>
 
 <div class="container hero text-dark" style="margin-top: 8em;">
-	<div class="row justify-content-evenly align-items-stretch">
-		<div class="col-lg-3 centered">
-			<h4>Application Repo</h4>
+	<div class="row justify-content-center align-items-center">
+		<div class="col-auto">
+			<h2>Additional Information and Resources</h2>
 		</div>
-		<div class="col-lg-3 centered">
-			<h4>Contract Repo</h4>
+	</div>
+
+	<div class="row justify-content-evenly align-items-stretch" style="margin-top: 4em;">
+		<div class="col-lg-3 info-card">
+			<a href="https://github.com/GaupaLabs/multisig-dapp" target="_blank"
+				><center
+					><h4>Application Repo</h4>
+					<img src="/static/folder.png" alt="dashboard icon" style="height:8em;" /></center
+				></a
+			>
 		</div>
-		<div class="col-lg-3 centered">
-			<h4>Gaupa Labs</h4>
+		<div class="col-lg-3 info-card">
+			<a
+				href="https://github.com/ElrondNetwork/elrond-wasm-rs/tree/master/contracts/examples/multisig"
+				target="_blank"
+				><center
+					><h4>Contract Code</h4>
+					<img src="/static/contract.png" alt="dashboard icon" style="height:8em;" /></center
+				></a
+			>
+		</div>
+		<div class="col-lg-3 info-card">
+			<a href="https://gaupalabs.com" target="_blank">
+				<center
+					><h4>Gaupa Labs</h4>
+					<img src="/static/twitter.png" alt="dashboard icon" style="height:8em;" /></center
+				></a
+			>
 		</div>
 	</div>
 </div>
 
-<!-- <div class="container hero-dark text-light card-background" style="margin-top: 8em;">
-	<div class="row justify-content-start align-items-start">
-		<div class="col">
-			<h3>Log in using your wallet.</h3>
-			<button class="btn btn-light" style="margin-top: 2em;"
-				><a
-					class="text-dark"
-					href="https://devnet-wallet.elrond.com/hook/login?callbackUrl=http://localhost:3000/dashboard"
-					>Login</a
-				></button
-			>
-		</div>
-	</div>
-</div> -->
 <style>
 	.dark-card {
 		border-radius: 2em;
@@ -122,5 +131,20 @@
 		background-size: contain;
 		background-repeat: no-repeat;
 		z-index: 100;
+	}
+
+	.info-card {
+		padding: 1em;
+		transition: transform 0.2s;
+	}
+
+	.info-card:hover {
+		transform: scale(1.05);
+		cursor: pointer;
+	}
+
+	a {
+		text-decoration: none;
+		color: var(--background-blue);
 	}
 </style>
